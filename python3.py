5961690f1fba2e0ac1ee76330e95fea6dcbffd83
sapolnut <newhungtf2@gmail.com>
2026-01-31 14:15:46 +0700

    Collection of Python

---
+++ python3.py
@@ -0,0 +1,54 @@
+"""
+#
+# Part: Collection of Python
+# Lists, Tuples, Sets, Dictionaries
+#
+"""
+
+"""
+#
+# Lists
+#
+"""
+this_list = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(this_list)
+print(this_list[1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+# Tuples
+#
+"""
+this_tuples = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(this_tuples)
+print(this_tuples[1])
+print(type(this_tuples))
+print(len(this_tuples))
+
+"""
+#
+# Sets
+#
+"""
+this_sets = ["apple","banana","cherry","orange","kiwi","melon","mango"]
+print(this_sets)
+print(this_sets[1])
+print(type(this_sets))
+print(len(this_sets))
+
+"""
+#
+# Dictionaries
+#
+"""
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 196
+}
+print(this_dict)
+print(this_dict["model"])
+print(type(this_dict))
+print(len(this_dict))
