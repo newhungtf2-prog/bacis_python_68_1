58e65eca4fdb8d247fbcd8f6ba980bf1580e03b1
sapolnut <newhungtf2@gmail.com>
2026-01-31 14:39:01 +0700

    Loop

---
+++ python5.py
@@ -0,0 +1,25 @@
+"""
+#
+# Loop
+# while Loop
+#
+"""
+i = 1
+while i < 5:
+    print(i)
+    if i ==3:
+        break
+    i += 1
+
+"""
+#
+# Loop
+# for Loop 
+#
+"""
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "banana":
+        break
+    
