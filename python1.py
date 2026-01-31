e480848806c6a6c1de149e57c43fafc0d007684c
sapolnut <newhungtf2@gmail.com>
2026-01-31 13:18:52 +0700

    python Variables

---
+++ python1.py
@@ -0,0 +1,45 @@
+"""
+#
+# Part: Python Comment
+#
+"""
+
+
+"""
+v = s / t
+ความเร็ว = ระยะหาง / เวลา
+v = ความเร็ว (m/s)
+s = ระยะหาง (m)
+t = เวลา (s)
+"""
+
+print("Hello, World:")
+
+"""
+#
+# Part: Python variables
+#
+"""
+x = 10
+y = "10"
+print(x, y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Variable Naming
+#
+"""
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400
+MYVAR = 500
+
+myVariableName = 900
+MyVariableName = 1000
+my_variable_name = 1100
